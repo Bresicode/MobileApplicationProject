324cd11d49c31b8adfaaa252bde47c0ee517ba9a
Bresicode <s0568782@htw-berlin.de>
2020-05-30 23:30:31 +0200

    Create LibraryControlImplTest.java

---
+++ EvilSlidesJava/src/library/test/LibraryControlImplTest.java
@@ -0,0 +1,24 @@
+package library.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryControlImplTest {
+
+    @Test
+    void addDeck() {
+    }
+
+    @Test
+    void removeDeck() {
+    }
+
+    @Test
+    void addSlide() {
+    }
+
+    @Test
+    void removeSlide() {
+    }
+}
