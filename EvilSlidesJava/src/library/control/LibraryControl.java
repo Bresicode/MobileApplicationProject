5412888e48c1c80b85e48fd2d6d7a212c221fc39
Bresicode <s0568782@htw-berlin.de>
2020-05-30 23:24:53 +0200

    created LibraryControl.java and LibraryControlImpl.java

--- EvilSlidesJava/src/library/control/LibraryControl.java
+++ EvilSlidesJava/src/library/control/LibraryControl.java
@@ -1,30 +1,35 @@
 package library.control;
 
 import library.data.Deck;
+import library.data.Library;
 import library.data.Slide;
 
 public interface LibraryControl {
     /**
-     * adds a deck to a deck list
+     * adds a deck to a library
+     * @param library the library the deck has to be added to
      * @param deck the deck that has to be added
      */
-    void addDeck(Deck deck);
+    void addDeck(Library library, Deck deck);
 
     /**
-     * removes a deck from the deck list
+     * removes a deck from library
+     * @param library the library the deck has to be removed
      * @param deck the deck that has to be removed
      */
-    void removeDeck(Deck deck);
+    void removeDeck(Library library, Deck deck);
 
     /**
-     * adds a slide to the slide list
+     * adds a slide to a deck
+     * @param deck the deck the slide has to be added to
      * @param slide the slide that has to be added
      */
-    void addSlide(Slide slide);
+    void addSlide(Deck deck, Slide slide);
 
     /**
-     * removes a slide from the slide list
+     * removes a slide from a deck
+     * @param deck the deck the slide has to be removed from
      * @param slide the slide that has to be removed
      */
-    void removeSlide(Slide slide);
+    void removeSlide(Deck deck, Slide slide);
 }
---
+++ EvilSlidesJava/src/library/control/LibraryControlImpl.java
@@ -0,0 +1,31 @@
+package library.control;
+
+import library.data.Deck;
+import library.data.Library;
+import library.data.Slide;
+
+public class LibraryControlImpl implements LibraryControl {
+    public LibraryControlImpl() {
+
+    }
+
+    @Override
+    public void addDeck(Library library, Deck deck) {
+        library.getDecks().add(deck);
+    }
+
+    @Override
+    public void removeDeck(Library library, Deck deck) {
+        library.getDecks().remove(deck);
+    }
+
+    @Override
+    public void addSlide(Deck deck, Slide slide) {
+        deck.getSlides().add(slide);
+    }
+
+    @Override
+    public void removeSlide(Deck deck, Slide slide) {
+        deck.getSlides().remove(slide);
+    }
+}
