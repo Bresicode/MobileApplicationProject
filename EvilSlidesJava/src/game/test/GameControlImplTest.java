29ab5177cc6158a04b652d86ec0707bcef8db2c0
Bresicode <s0568782@htw-berlin.de>
2020-05-30 23:30:26 +0200

    Create GameControlImplTest.java

---
+++ EvilSlidesJava/src/game/test/GameControlImplTest.java
@@ -0,0 +1,37 @@
+package game.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameControlImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addPlayer() {
+    }
+
+    @Test
+    void removePlayer() {
+    }
+
+    @Test
+    void chooseWinner() {
+    }
+
+    @Test
+    void addSlideToPlayerHand() {
+    }
+
+    @Test
+    void removeSlideFromPlayerHand() {
+    }
+
+    @Test
+    void raisePlayerScore() {
+    }
+}
