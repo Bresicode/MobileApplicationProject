4ea1bf301ab2642957e7d24efa2096cd8f41f243
Bresicode <s0568782@htw-berlin.de>
2020-06-03 17:18:04 +0200

    removed networkcontrol from view as much as possible

--- EvilCards/app/src/main/java/com/example/evilslides/game/view/GameActivity.java
+++ EvilCards/app/src/main/java/com/example/evilslides/game/view/GameActivity.java
@@ -1,12 +1,5 @@
 package com.example.evilslides.game.view;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.IntentFilter;
-import android.net.wifi.WifiManager;
-import android.net.wifi.p2p.WifiP2pConfig;
-import android.net.wifi.p2p.WifiP2pDevice;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,46 +7,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.evilslides.R;
 import com.example.evilslides.game.network.DiscoverPeersListener;
+import com.example.evilslides.game.network.NetworkController;
 import com.example.evilslides.game.network.WifiP2pBroadcastReceiver;
 import com.example.evilslides.library.control.LibraryAccessor;
 
 public class GameActivity extends AppCompatActivity {
     public static final String TAG = "GameActivity";
 
-    WifiP2pManager wifiP2pManager;
-    WifiP2pManager.Channel channel;
-    BroadcastReceiver receiver;
-    IntentFilter intentFilter;
-    WifiP2pDevice device;
-    WifiP2pConfig config;
-    WifiManager wifiManager;
+
     LibraryAccessor libraryAccessor;
+    private WifiP2pBroadcastReceiver receiver;
+    private NetworkController nc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
+        nc = new NetworkController(this);
         libraryAccessor = new LibraryAccessor(this);
-        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-
-        wifiP2pManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        channel = wifiP2pManager.initialize(this, getMainLooper(), null);
-        receiver = new WifiP2pBroadcastReceiver(wifiP2pManager, channel, this);
-
-        intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
+        receiver = new WifiP2pBroadcastReceiver(nc.getWifiP2pManager(), nc.getChannel(), this);
     }
 
     /* register the broadcast receiver with the intent values to be matched */
     @Override
     protected void onResume() {
         super.onResume();
-        registerReceiver(receiver, intentFilter);
+        registerReceiver(receiver, nc.getIntentFilter());
     }
 
     /* unregister the broadcast receiver */
@@ -64,26 +43,14 @@ public class GameActivity extends AppCompatActivity {
     }
 
     public void wifi(View view) {
-        if (!wifiManager.isWifiEnabled()) {
-            wifiManager.setWifiEnabled(true);
+        if (!nc.getWifiManager().isWifiEnabled()) {
+            nc.getWifiManager().setWifiEnabled(true);
         }
-        wifiP2pManager.discoverPeers(channel, new DiscoverPeersListener());
+        nc.getWifiP2pManager().discoverPeers(nc.getChannel(), new DiscoverPeersListener());
     }
 
     public void connect(View view) {
-        config = new WifiP2pConfig();
-        config.deviceAddress = device.deviceAddress;
-        wifiP2pManager.createGroup(channel, new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() {
 
-            }
-
-            @Override
-            public void onFailure(int reason) {
-
-            }
-        });
     }
 
 }
